f2423ce4169041fb15578da1d3643ad4a8135f49
s-hiroshi <s-hiroshi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-17 09:01:47 +0000

    add text field
    git-svn-id: https://plugins.svn.wordpress.org/min-calendar/tags/1.4.6@757549 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-draw-calendar.php
+++ includes/class-draw-calendar.php
@@ -14,18 +14,20 @@ class MC_Draw_Calendar {
      */
     public static function draw( $post_id )
     {
-        $year   = (int) get_post_meta( $post_id, 'year' , true );
-        $month  = (int) get_post_meta( $post_id, 'month', true );
-        $res   = MC_Date::get_days( $year, $month , false );
-        $days = $res[ 'days' ];
+        $year        = (int) get_post_meta( $post_id, 'year' , true );
+        $month       = (int) get_post_meta( $post_id, 'month', true );
+        $res         = MC_Date::get_days( $year, $month , false );
+        $days        = $res[ 'days' ];
         $day_of_week = $res[ 'day_of_week' ];
         $total = count( $days );
 
         for ( $i = 1; $i <= $total; $i++ ) {
-            $key_date         = 'date-' . $i;
-            $key_post         = 'post-' . $i;
-            $date[ $i ]       = get_post_meta( $post_id, $key_date, true );
-            $relatepost[ $i ] = get_post_meta( $post_id, $key_post, true );
+            $key_date           = 'date-' . $i;
+            $key_post           = 'post-' . $i;
+            $key_text           = 'text-' . $i;
+            $date[ $i ]         = get_post_meta( $post_id, $key_date, true );
+            $relate_posts[ $i ] = get_post_meta( $post_id, $key_post, true );
+            $texts[ $i ]        = get_post_meta( $post_id, $key_text, true );
         }
 
         $html = self::make(
@@ -33,7 +35,8 @@ class MC_Draw_Calendar {
             $month,
             $date,
             $day_of_week,
-            $relatepost
+            $relate_posts,
+            $texts
         );
         return $html;
     }
@@ -44,12 +47,13 @@ class MC_Draw_Calendar {
      *
      * @param string $y year yyyy
      * @param string $m month 1～13
-     * @param string $date 日付情報(配列)
-     * @param array $day_of_week 曜日のラベル
-     * @param array $relatepost 曜日に紐づいた投稿
+     * @param array  $date 日付情報
+     * @param array  $day_of_week 曜日のラベル
+     * @param array  $relate_posts 曜日に紐づいた投稿
+     * @param array  $texts テキスト
      * @return string 曜日のマークアップ
      */
-    private static function make( $y, $m, $date, $day_of_week, $relatepost )
+    private static function make( $y, $m, $date, $day_of_week, $relate_posts, $texts )
     {
         // $y 年 $m 月
         $t     = mktime( 0, 0, 0, $m, 1, $y ); // $y年$m月1日のUNIXTIME
@@ -89,6 +93,7 @@ HTML;
             }
             // 日付が有効な場合の処理
             if ( ( 0 < $i ) && ( $i <= $n) ) {
+                // 日付情報
                 // $key = 'date-' . $i;
                 $option = $date[ $i ];
                 $context   = '';
@@ -96,50 +101,44 @@ HTML;
                 // 曜日の取得
                 $hizuke = mktime( 0, 0, 0, $m, $i, $y ); //$y年$m月$i日のUNIXTIME
                 $youbi  = date( 'w', $hizuke ); // 1日の曜日（0:日～6:土）
+
+                // mc-valueの設定
                 if ( $option === 'mc-value-1st' ) {
-                    $html .= ' class="mc-bgcolor-1st';
-                    if ( 0 === (int) $youbi ) {
-                        $html .= ' mincalendar-td-sun';
-                    } else if ( 6 === (int) $youbi ) {
-                        $html .= ' mincalendar-td-sat';
-                    }
-                    $html .= '"';
+                    $html .= self::set_holiday( '1st', $html, $youbi);
                     $context = ( "\x20" === $options[ 'mc-value-1st' ] ) ? '&nbsp;' : $options[ 'mc-value-1st' ];
                 } else if ( $option === 'mc-value-2nd' ) {
-                    $html .= ' class="mc-bgcolor-2nd';
-                    if ( 0 === (int) $youbi ) {
-                        $html .= ' mincalendar-td-sun';
-                    } else if ( 6 === (int) $youbi ) {
-                        $html .= ' mincalendar-td-sat';
-                    }
-                    $html .= '"';
+                    $html .=  self::set_holiday( '2nd', $html, $youbi);
                     $context = ( "\x20" === $options[ 'mc-value-2nd' ] ) ? '&nbsp;' : $options[ 'mc-value-2nd' ];
                 } else if ( $option === 'mc-value-3rd' ) {
-                    $html .= ' class="mc-bgcolor-3rd"';
-                    if ( 0 === (int) $youbi ) {
-                        $html .= ' mincalendar-td-sun';
-                    } else if ( 6 === (int) $youbi ) {
-                        $html .= ' mincalendar-td-sat';
-                    }
-                    $html .= '"';
+                    $html .= self::set_holiday( '3rd', $html, $youbi);
                     $context = ( "\x20" === $options[ 'mc-value-3rd' ] ) ? '&nbsp;' : $options[ 'mc-value-3rd' ];
-                } else if ( 0 === (int) $youbi ) {
-                    $html .= ' class="mincalendar-td-sun"';
-                } else if ( 6 === (int) $youbi ) {
-                    $html .= ' class="mincalendar-td-sat"';
                 }
-                if ( is_numeric( $relatepost[ $i ] ) ) {
-                    $relate = get_post( $relatepost[ $i ] );
-                    $link   = '<a target="_blank" href="' . get_permalink( $relatepost[ $i ] ) .'">' . $relate->post_title . '</a>';
+
+                // 紐づけた投稿
+                if ( is_numeric( $relate_posts[ $i ] ) ) {
+                    $relate = get_post( $relate_posts[ $i ] );
+                    $link   = '<a target="_blank" href="' . get_permalink( $relate_posts[ $i ] ) .'">' . $relate->post_title . '</a>';
                 } else {
                     $link = '';
                 }
-                $html .= '>' . $i . '<br>' . esc_html( $context ) . '<br>';
+
+                // テキスト
+                if ( '' !== $texts[ $i ] ) {
+                    $text = esc_html( $texts[ $i ] );
+                } else {
+                    $text = '';
+                }
+
+                // context
+                $html .= '>' . $i . '<br>' . esc_html( $context ) ;
                 if ( false === empty( $link ) ) {
-                    $html .=  $link;
+                    $html .= '<br>' . $link;
+                }
+                if ( false === empty( $text ) ) {
+                    $html .= '<br>' . $text;
                 }
                 $html .= '</td>' . PHP_EOL;
-            } else {
+            } else { // 日付が有効な場合の処理
                 $html .= '<td>&nbsp;</td>' . PHP_EOL;
             }
             if ( ( ( $i + $w ) % 7 ) == 0 ) {
@@ -153,6 +152,25 @@ HTML;
 
         // $htmlはエスケープ済み
         return $html;
+    }
+
+
+    /**
+     * @param $index (1st, 2nd, 3rd)
+     * @param $youbi 曜日 0:日, 6:土曜日
+     * @return string マークアップ
+     */
+    private function set_holiday( $index, $youbi )
+    {
+        $html = ' class="mc-bgcolor-' . $index;
+        if ( 0 === (int) $youbi ) {
+            $html .= ' mincalendar-td-sun';
+        } else if ( 6 === (int) $youbi ) {
+            $html .= ' mincalendar-td-sat';
+        }
+        $html .= '"';
+        return $html;
+
     }
 
 }
